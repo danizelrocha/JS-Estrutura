0fd64c246530d7b743c533062ae14b6632ecd504
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-08 10:24:04 -0300

    propety

--- index.js
+++ index.js
@@ -48,10 +48,23 @@ let itemPrice = 3
 let itemAvailable = true
 let itemColor = 'Red'
 
+
+// objeto
+/* let pen = {
+    itemName: 'Pen',
+    itemPrice: 3,
+    itemAvailable: true,
+    itemColor: 'Red' */
+}
+console.log(pen)
+
+//alterar propetis ou itens
+
 let pen = {
     itemName: 'Pen',
     itemPrice: 3,
     itemAvailable: true,
     itemColor: 'Red'
 }
-console.log(pen)
+
+pen.itemColor = 'blue'
