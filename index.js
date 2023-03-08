8eda5e91f233a33ec6b905e2af9ea1c7f1360e2b
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-08 08:58:26 -0300

    js

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
+
+// Aula 1 let - hoje se usa let no lugar de var
 console.log('Javascript Funcionando');
 
 let firstName ='Daniel';
 let lastName = 'Rocha';
-let sexo = 'Masculino'
+let sexo = 'Masculino';
+let cor = 'Branco';
+let time = 'São Paulo';
 console.log(firstName);
 console.log(lastName);
 console.log(sexo);
+console.log(cor);
+console.log(time);
+
+// Aula 2 const não é ubistituida por ser considerada principal tem um peso maior que let 
+
+const price = 30;
+// price = 20;
+console.log(price);
+
+// aula 3 ASI - automatic semicolon insertion - ponto evirgula o  dosi pontosque usar?
+
+let Name = 'Laura'
+console.log(Name)
+
