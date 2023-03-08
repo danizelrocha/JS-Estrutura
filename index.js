3ab2482f51d1b5036a4b78326e0ce40b29b31996
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-07 21:18:09 -0300

    html

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 console.log('Javascript Funcionando');
+
+let firstName ='Daniel';
+let lastName = 'Rocha';
+let sexo = 'Masculino'
+console.log(firstName);
+console.log(lastName);
+console.log(sexo);
