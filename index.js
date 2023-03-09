60e1446cbbde4bc32a012c9c02e578ba92d3c015
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-09 09:51:36 -0300

    Operadores

--- index.js
+++ index.js
@@ -110,7 +110,61 @@ let num2 = 2
 console.log(num1 * num2) 
 */
 
+/* let num1 = 3
+let num2 = 2
+
+console.log(num1 / num2) */
+/* 
 let num1 = 3
 let num2 = 2
 
-console.log(num1 / num2)
+console.log(num1 % num2) */ // o simbolo de procentagem trabalha com o resto da divisão 
+
+
+/* let num1 = 3
+let num2 = 2
+
+console.log(num1 ** num2) */ //o ** trabalha com exponencial sendo pelo 
+                         // exemplo 3*3 =9 já que o 2 virou o exponencial de 3
+
+                         
+/*
+----------Operadores de---------- 
+encremento são representados pelos operadores : ++
+decremento são representados pelos operadores : --
+*/
+
+
+/* let num1 = 3
+let num2 = 2
+
+let total = num1 + num2
+                         
+console.log(++total) // soma mais 1, e mostar
+console.log(total)
+
+let num1 = 3
+let num2 = 2
+
+let total = num1 + num2
+                         
+console.log(total++) // soma mais 1, mas não mostra.
+console.log(total)
+                         
+
+let num1 = 3
+let num2 = 2
+
+let total = num1 + num2
+                         
+console.log(--total) //subitrai 1, e mostra 
+console.log(total)
+
+let num1 = 3
+let num2 = 2
+
+let total = num1 + num2
+                         
+console.log(total--) // subitrai 1, e não mostra
+console.log(total)
+                         */
