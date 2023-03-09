a83ed3a8cb1d7972d3146c76cf62a7e2d2b98724
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-09 10:17:31 -0300

    Implementos

--- index.js
+++ index.js
@@ -189,11 +189,31 @@ let num1 = 3 */
 
 //-------Comparação
 
-let num1 = 3
+/* let num1 = 3
 console.log(num1 > 0)
 console.log(num1 < 0)
 console.log(num1 >= 3)
 console.log(num1 <= 3)
 
 console.log(num1 === 3)
-console.log(num1 !== 3)
+console.log(num1 !== 3) */
+
+
+//São iguais em valor e tipo
+/* let num1 = 1
+let num2 = 1
+
+console.log(num1 === num2) */
+
+// São de valores iguais, mas de tipos diferentes um é uma stirng e o outro um numero 
+/* let num1 = '1'
+let num2 = 1
+
+console.log(num1 === num2) */
+
+
+let num1 = '2'
+let num2 = 2
+
+console.log(num1 === num2)
+console.log(num1 == num2)// observa se são iguai no valor
