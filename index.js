30907f998f7e1b4cfd44d65c08b916878c8db7be
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-03-09 10:02:25 -0300

    Comparação

--- index.js
+++ index.js
@@ -184,3 +184,16 @@ let num1 = 3 */
 
 /* num1 /= 20  */
 /* console.log(num1) */
+
+
+
+//-------Comparação
+
+let num1 = 3
+console.log(num1 > 0)
+console.log(num1 < 0)
+console.log(num1 >= 3)
+console.log(num1 <= 3)
+
+console.log(num1 === 3)
+console.log(num1 !== 3)
