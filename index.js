2a78277fd974ae063559a48b3609461e616a92c5
DANIEL ALVES DA ROCHA <101830720+danizelrocha@users.noreply.github.com>
2023-05-22 10:09:34 -0300

    Argumentos em funções

--- index.js
+++ index.js
@@ -678,7 +678,7 @@ console.log(tempNegative) */
 
 
   //----Fuction Declaretion------//
-  movie()
+/*   movie()
 
   function movie () {
     console.log('Tales From The Loop')
@@ -692,4 +692,16 @@ console.log(tempNegative) */
 
   car()
   const truck = car
-  truck() 
+  truck()  */
+
+
+  //-----Argumentos em fuções----//
+
+  function price() {
+    let total = 0
+    for (let value of arguments)
+        total += value
+    return total
+  }
+
+  console.log(price(10, 20, 30, 40))
